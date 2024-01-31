4a097ba504e545c7ce50f5ec7c65e33899866552
Gamaliel Pereira <finestweber@gmail.com>
2024-01-30 22:21:47 -0300

    WebConfig deploy

---
+++ src/main/java/com/finestweber/msemail/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.finestweber.msemail.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/send-email")
+                .allowedOrigins("https://seu-front-end.com")
+                .allowedMethods("GET", "POST")
+                .allowCredentials(true);
+    }
+}
+
