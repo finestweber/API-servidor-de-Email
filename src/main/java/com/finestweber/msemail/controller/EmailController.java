6182b7d5f1fcbf4e9646c98c397da0ddfecf476a
Gamaliel Pereira <finestweber@gmail.com>
2024-01-30 21:25:46 -0300

    CrossOrigin

--- src/main/java/com/finestweber/msemail/controller/EmailController.java
+++ src/main/java/com/finestweber/msemail/controller/EmailController.java
@@ -10,10 +10,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(origins = "*")
 @RestController
 public class EmailController {
     @Autowired
