bc57f75ea8b9f92fd184383da4b6e96a17844e9a
Gamaliel Pereira <finestweber@gmail.com>
2024-02-03 13:25:13 -0300

    Finalizado

--- src/main/java/com/finestweber/msemail/controller/EmailController.java
+++ src/main/java/com/finestweber/msemail/controller/EmailController.java
@@ -3,18 +3,59 @@ package com.finestweber.msemail.controller;
 import com.finestweber.msemail.dto.EmailDto;
 import com.finestweber.msemail.model.EmailModel;
 import com.finestweber.msemail.service.EmailService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/emails") // Prefixo para todas as rotas do controlador
+public class EmailController {
+
+    @Autowired
+    EmailService emailService;
+
+    // Endpoint para enviar um e-mail
+    @PostMapping("/send-email")
+    public ResponseEntity<EmailModel> sendingEmail(@Valid @RequestBody EmailDto emailDto) {
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+    }
+
+    // Endpoint para obter todos os e-mails
+    @GetMapping("/all")
+    public ResponseEntity<List<EmailModel>> getAllEmails() {
+        List<EmailModel> allEmails = emailService.getAllEmails();
+        if (!allEmails.isEmpty()) {
+            return ResponseEntity.ok(allEmails); // 200 OK
+        } else {
+            return ResponseEntity.notFound().build(); // 404 Not Found
+        }
+    }
+}
+
+/*
+* import com.finestweber.msemail.dto.EmailDto;
+import com.finestweber.msemail.model.EmailModel;
+import com.finestweber.msemail.service.EmailService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-@CrossOrigin(origins = "https://api-servidor-de-email-production.up.railway.app")
+
+@CrossOrigin(origins = "*")
 @RestController
 public class EmailController {
 
@@ -22,10 +63,20 @@ public class EmailController {
     EmailService emailService;
 
     @PostMapping("/send-email")
-    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto){
+    public ResponseEntity<EmailModel> sendingEmail(@RequestParam("from") String from,
+                                                   @RequestParam("to") String to,
+                                                   @RequestParam("subject") String subject,
+                                                   @RequestParam("text") String text){
         EmailModel emailModel = new EmailModel();
-        BeanUtils.copyProperties(emailDto,emailModel);
+        emailModel.setEmailFrom(from);
+        emailModel.setEmailTo(to);
+        emailModel.setSubject(subject);
+        emailModel.setText(text);
+
         emailService.sendEmail(emailModel);
+
         return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
     }
 }
+
+* */
--- src/main/java/com/finestweber/msemail/service/EmailService.java
+++ src/main/java/com/finestweber/msemail/service/EmailService.java
@@ -10,15 +10,18 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class EmailService {
-    @Autowired
     EmailRepository emailRepository;
-    @Autowired
-    private JavaMailSender emailSender;
+    private JavaMailSender javaMailSender;
 
-    public EmailModel sendEmail(EmailModel emailModel) {
+    public EmailService(EmailRepository emailRepository, JavaMailSender javaMailSender) {
+        this.emailRepository = emailRepository;
+        this.javaMailSender = javaMailSender;
+    }
+    public EmailModel sendEmail(EmailModel emailModel){
         emailModel.setSendDateEmail(LocalDateTime.now());
         try{
             SimpleMailMessage message = new SimpleMailMessage();
@@ -26,12 +29,16 @@ public class EmailService {
             message.setTo(emailModel.getEmailTo());
             message.setSubject(emailModel.getSubject());
             message.setText(emailModel.getText());
-            emailSender.send(message);
+            javaMailSender.send(message);
             emailModel.setStatusEmail(StatusEmail.SENT);
         }catch (MailException e){
             emailModel.setStatusEmail(StatusEmail.ERROR);
         }finally {
             return emailRepository.save(emailModel);
         }
+    }
+
+    public List<EmailModel> getAllEmails() {
+        return emailRepository.findAll();
     }
 }
